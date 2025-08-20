bdc608260c64cade0714dc84122ec73735c63a90
mickche <174184243+mickche@users.noreply.github.com>
2025-08-20 09:08:02 +0300

    print methods

---
+++ methods.py
@@ -0,0 +1,20 @@
+from itertools import batched
+
+obj = "example_str"
+
+methods = []
+for item in dir(obj):
+    # Get the actual attribute object
+    attribute = getattr(obj, item)
+    # Check if it's callable (a method) and not a dunder method
+    if callable(attribute) and not item.startswith('__'):
+        methods.append(item)
+
+print("====="*10)
+print(f"===== There are {len(methods)} methods in the 'str' class ====")
+n = 5
+print("===== here they are, split in batches of {n} =====")
+print("====="*10)
+for batch in batched(methods, n):
+    print(batch)
+print("====="*10)
