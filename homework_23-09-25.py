bb3bc9281282911cbd6c5f2f949b4fc4c0c2ae92
mickche <174184243+mickche@users.noreply.github.com>
2025-09-21 22:50:45 +0300

    21.09.25

---
+++ homework_23-09-25.py
@@ -0,0 +1,35 @@
+# 1.
+# b**2 - 4*a*c формула
+def num_of_solution(a,b,c) -> int:
+    if b**2 < 4*a*c:
+        return 0
+    elif b**2 == 4*a*c:
+        return 1
+    else:
+        return 2
+# sol = num_of_solution(-100,0,1)
+# print(f'Equation have {sol} solution')
+
+# 2.
+s = input('>>>')
+def valid(s: str) -> bool:
+    if s == '':
+        return False
+    vd = ('(','[','{')
+    zd = (')',']','}')
+    # Це створює словник відповідних дужок
+    # ключі - відкриваючі, значення - закриваючі
+    d_map = dict(zip(vd,zd))
+
+    stack = []
+    if s[0] in zd:
+        return False
+    for i in s:
+        if i in vd:
+            stack.append(i)
+        elif i in zd:
+            top = stack.pop()
+            if d_map[top] != i:
+                return False
+    return True
+print(valid(s))
